1f7774f9d8fc23abcc971b8ea565af4fb7c6ce62
sekema954 <sekema954@gmail.com>
2025-11-04 01:37:36 -0500

    Added courses link to nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
     { id: 2, title: 'Services', path: '/services' },
     { id: 3, title: 'Blog', path: '/blog' },
     { id: 4, title: 'Videos', path: '/videos' },
-    //{ id:5, title:"Courses", path:'/courses'},
+    { id:5, title:"Courses", path:'/courses'},
   ];
 
   const handleNavClick = () => setMenuOpen(false);
